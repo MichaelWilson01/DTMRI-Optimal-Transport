de11a28a9f2c97790e315ab4d76f21fe8a276105
MichaelWilson01 <mkw19c@my.fsu.edu>
2022-09-24 22:26:56 -0400

    Update OptimalTransport.py

--- Code/OptimalTransport.py
+++ Code/OptimalTransport.py
@@ -7,6 +7,22 @@ Created on Sat Sep 24 19:37:55 2022
 
 
 class FOT_optimizer:
+    """
+    Initializes FOT instance
+    
+    data should have shape: (timeSteps, numFunc) 
+    or (timeSteps, numFunc, numDim) if numDim>1
+            
+    Inputs:
+        - source: set of functions to be transported, 
+        - target: set of functions being transported to
+        
+    Optional Inputs:
+        - lr: learning rate for gradient descent
+        - eta: regularization for Lambda
+        - gamma_h: entropy coefficient for Sinkhorn
+         
+    """
     
     def __init__(self, source, target, 
                  lr=1e-9, eta = 1, gamma_h = 5):
@@ -44,7 +60,8 @@ class FOT_optimizer:
         pass
     
     # steps of optimization
-    def get_cost(self):
+    def get_cost_matrix(self):
+        #return costMatrix
         pass
     
     def get_pi_sinkhorn(self):
@@ -62,4 +79,12 @@ import numpy as np
         
 FOT = FOT_optimizer(np.random.rand(3,3,3),np.random.rand(3,3,3))
 
+print(FOT.lr)
 print(FOT.eta)
+print(FOT.gamma_h)
+print(FOT.X)
+print(FOT.Y)
+print(FOT.U)
+print(FOT.V)
+
+
