0c7eef6204e8692116bdb8796e25927b8d174bd6
MichaelWilson01 <mkw19c@my.fsu.edu>
2022-09-27 16:23:31 -0400

    Update OptimalTransport.py

--- Code/OptimalTransport.py
+++ Code/OptimalTransport.py
@@ -30,69 +30,67 @@ class FOT_optimizer:
         self.X = self.data_shaper(X)
         self.Y = self.data_shaper(Y)
         
+        self.max_iter = max_iter
+        
         self.U = self.get_basis(self.X)
         self.V = self.get_basis(self.Y)
         
-        self.k_x = self.X.shape[1]
-        self.k_y = self.Y.shape[1]
+        self.k_x = self.X.shape[0]
+        self.k_y = self.Y.shape[0]
         
         self.Lambda = None
         self.T = None
         self.C = None
         self.Pi = None
         
-        self.max_iter = max_iter
-        
         self.lr = lr
         self.eta = eta
         self.gamma_h = gamma_h
-               
-    
+                   
     # methods for initializing FOT instance
     def data_shaper(self, functionData):
         if len(np.shape(functionData))==3:
             numFunc,numDim,timeSteps = np.shape(functionData)
-            return np.reshape(functionData, (numFunc, numDim*timeSteps))
+            return np.reshape(functionData, (numFunc, numDim*timeSteps)).T
         elif len(np.shape(functionData))>3: 
             raise ValueError("Data should have shape (numFunc,timeSteps), or (numFunc, numDim, timeSteps) if numDim>1. Instead, data has shape "+str(np.shape(functionData)))
         else: 
-            return functionData
+            return functionData.T
        
     def get_basis(self,functionData):
         #Get PCA Basis
         muF = np.mean(functionData,0)
         F_centered = functionData - muF
-        C = F_centered.T@F_centered
+        C = F_centered@F_centered.T
         U = np.linalg.svd(C)[0]        
         return U
      
-    # set parameters for optimization manually
     def set_parameters(self, **kwargs):
+        """
+        Manually update parameter values
+             
+        """
         for key, value in kwargs.items():
             if key in vars(self):
                 print(key + " updated from " + str(getattr(self, key)) + " to " + str(value))
                 setattr(self, key, value)
             else:
-                print(key + " is not a model parameter.")
-    
+                print(key + " is not a class attribute.")
     
     # methods for optimization
     def optimize_step(self):
-        #
-        if FOT.Lambda==None: self.initialize_lambda()
         # Calculate Cost as a function of Lambda
-        self.get_cost_matrix()
+        self.update_cost_matrix()
         # Estimate Pi with Lambda fixed, with sinkhorn
-        self.get_pi_sinkhorn()
+        self.update_pi_sinkhorn()
         # Updata Lambda with Pi fixed
         self.update_lambda()  
     
-    # methods for optimization    
     def initialize_lambda(self):
         temp = self.V.T @ self.U
         self.Lambda = temp[0:self.k_y,0:self.k_x]  
     
-    def get_cost_matrix(self):
+    def update_cost_matrix(self):
         T = self.V @ self.Lambda @ self.U.T
         TX = T@self.X
         C = np.zeros((TX.shape[0],self.Y.shape[0]))
@@ -100,17 +98,15 @@ class FOT_optimizer:
             C[a] = np.mean(np.power(self.Y - TX[a],2),1)
         self.C = C
     
-    def get_pi_sinkhorn(self,C,g):
+    def update_pi_sinkhorn(self):
         pass
-    
-    def get_gradient(self):
-        pass
-    
+  
     def update_lambda(self):
         pass
     
     def optimize(self):
-        while not stopCondition:
+        if FOT.Lambda==None: self.initialize_lambda()       
+        for i in range(self.max_iter):
             self.optimize_step()
     
     
@@ -119,9 +115,9 @@ import numpy as np
 
 a=1
 
-numFunc = 1000
+numFunc = 10
 numDim = 3
-timeSteps = 150
+timeSteps = 5
 
 X=np.zeros((numFunc,numDim,timeSteps))
 Y=np.zeros((numFunc,numDim,timeSteps))
